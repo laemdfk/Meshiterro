96c9f54a74c7e0ed9bb63f7a5564a694f15ec00b
laemdfk <kappa021618@icloud.com>
2022-07-24 16:56:19 +0000

    [Add]ログイン/サインイン/ログアウト実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    # ユーザー登録(sign_up)の際に、ユーザー名(name)のデータ操作を許可する
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
+
