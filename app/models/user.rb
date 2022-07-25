d750d1da13168d2a7f8fa4d0b7e212bd6b810478
laemdfk <kappa021618@icloud.com>
2022-07-25 12:52:48 +0000

    [Add]アソシエーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+        # アソシエーション設定。1:N の「1」側にあたるモデルに、has_many を記載する必要がある 
+          has_many :post_images, dependent: :destroy
 end
