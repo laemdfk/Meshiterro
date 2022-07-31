2159fdfbfb5f497db7f0040a773b2aa398b4b37a
laemdfk <kappa021618@icloud.com>
2022-07-31 12:30:06 +0000

    [Add]users#controller作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/edit'
   devise_for :users
   root to: 'homes#top'
   
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :users, only: [:show, :edit]
   
   get 'homes/about'
   
