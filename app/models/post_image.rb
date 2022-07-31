e9a0a4baadc1ee7e2cdb7e302e26e487196779a5
laemdfk <kappa021618@icloud.com>
2022-07-31 14:50:27 +0000

    [Add]コメント機能実装

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -3,16 +3,17 @@ class PostImage < ApplicationRecord
   # has_many_attachedとすると、一つの投稿に対し、複数の写真が関連づけられる。
 
   belongs_to :user
+  has_many :post_comments, dependent: :destroy
 
 
 # 画像が設定されない場合はapp/assets/imagesに格納されている
 # no_image.jpgという画像をデフォルト画像としてActiveStorageに格納し、格納した画像を表示する
 
-  def get_image
+ def get_image
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
     image
-  end
+ end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/edit'
   devise_for :users
   root to: 'homes#top'
-  
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
-  resources :users, only: [:show, :edit]
-  
-  get 'homes/about'
-  
-  
+   get 'homes/about'
+
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resources :post_comments, only: [:create]
+  end
+   resources :users, only: [:show, :edit, :update]
+
+ 
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
